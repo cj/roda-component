247eb00a1e5cab48840a97584e9b48ad67119024
cj <cjlazell@gmail.com>
2014-12-30 21:45:49 -0600

    chatboc working

--- test/dummy/components/theme.rb
+++ test/dummy/components/theme.rb
@@ -2,19 +2,17 @@ class BoxComponent < Roda::Component
   name :theme
   html "../public/AdminLTE-master/index.html"
 
-  # setup do |dom|
-  #   tmpl :about_title, dom.at_css('#about h2.about-title')
-  # end
-
   setup do |dom|
     # remove sales box
     dom.at('#revenue-chart').xpath('//div[contains(@class,"nav-tabs-custom")]').first['style'] = 'display: none;'
     # dom.css('script[src*=morris]').remove
 
     # set chat box and grab chatbox row
+    box      = dom.find('#chat-box').ancestors('.box').first
     chat_box = dom.at('#chat-box')
-    tmpl :chat_box_row, chat_box.css('.item').first
+    tmpl :chat_box_row, box.css('.item').first
     chat_box.css('.item').remove
+    box.at('box-tools').remove
   end
 
   def display data, &block
@@ -25,11 +23,11 @@ class BoxComponent < Roda::Component
 
   # add message to chat box
   on :ready, '.box-footer button' do |el|
-    chat_box = el.closest('.box').find('#chat-box')
-    box      = chat_box.closest('.box')
+    box = el.closest('.box').find('#chat-box').closest('.box')
 
     box.find('.box-footer button').on('click') { add_chat_row_to box }
     box.find('.box-footer input').on('keydown') do |evt|
+      # on enter add chat row
       if evt.which == 13
         add_chat_row_to box
       end
@@ -41,14 +39,20 @@ class BoxComponent < Roda::Component
   def add_chat_row_to box
     chat_box = box.find('#chat-box')
     input    = box.find('.box-footer input')
+    value    = input.val
+    row      = tmpl(:chat_box_row)
 
-    row = tmpl(:chat_box_row)
-    # we don't need attachments
-    row.find('.attachment').remove
-    row.find('.message').html = input.val
-    input.prop 'value', ''
+    if value.length > 0
+      # we don't need attachments
+      row.find('.attachment').remove
+      row.find('.message').html = value
+      input.prop 'value', ''
 
-    chat_box.append row.dom
-    chat_box.scroll_top(chat_box.height + 2000)
+      chat_box.append row.dom
+      # make sure we always scroll to the bottom
+      chat_box.scroll_top(chat_box.height + 99999)
+    else
+      # error message: input can't be blank
+    end
   end
 end
